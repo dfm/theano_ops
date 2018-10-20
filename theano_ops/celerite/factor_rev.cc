46b9234557b732b4d162ea5f733e2dbd14df4058
Dan F-M <foreman.mackey@gmail.com>
2018-10-20 09:02:13 -0400

    adding demo notebook

--- theano_ops/celerite/factor_rev.cc
+++ theano_ops/celerite/factor_rev.cc
@@ -51,15 +51,15 @@ int APPLY_SPECIFIC(factor_rev)(
     return 1;
   }
 
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_0, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > U((DTYPE_INPUT_0*)PyArray_DATA(input0), N, J);
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_1, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > P((DTYPE_INPUT_1*)PyArray_DATA(input1), N-1, J);
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_2, Eigen::Dynamic, 1> > d((DTYPE_INPUT_2*)PyArray_DATA(input2), N);
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_3, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > W((DTYPE_INPUT_3*)PyArray_DATA(input3), N, J);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_0, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> >  U((DTYPE_INPUT_0*)PyArray_DATA(input0), N, J);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_1, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> >  P((DTYPE_INPUT_1*)PyArray_DATA(input1), N-1, J);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_2, Eigen::Dynamic, 1> >                             d((DTYPE_INPUT_2*)PyArray_DATA(input2), N);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_3, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> >  W((DTYPE_INPUT_3*)PyArray_DATA(input3), N, J);
   Eigen::Map<Eigen::Matrix<DTYPE_INPUT_4, Eigen::Dynamic, CELERITE_J2, CELERITE_J_ORDER> > S((DTYPE_INPUT_4*)PyArray_DATA(input4), N, J*J);
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_5, Eigen::Dynamic, 1> > bd((DTYPE_INPUT_5*)PyArray_DATA(input5), N);
-  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_6, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > bW((DTYPE_INPUT_6*)PyArray_DATA(input6), N, J);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_5, Eigen::Dynamic, 1> >                             bd((DTYPE_INPUT_5*)PyArray_DATA(input5), N);
+  Eigen::Map<Eigen::Matrix<DTYPE_INPUT_6, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> >  bW((DTYPE_INPUT_6*)PyArray_DATA(input6), N, J);
 
-  Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_0, Eigen::Dynamic, 1> > ba((DTYPE_OUTPUT_0*)PyArray_DATA(*output0), N);
+  Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_0, Eigen::Dynamic, 1> >                            ba((DTYPE_OUTPUT_0*)PyArray_DATA(*output0), N);
   Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_1, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > bU((DTYPE_OUTPUT_1*)PyArray_DATA(*output1), N, J);
   Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_2, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > bV((DTYPE_OUTPUT_2*)PyArray_DATA(*output2), N, J);
   Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_3, Eigen::Dynamic, CELERITE_J, CELERITE_J_ORDER> > bP((DTYPE_OUTPUT_3*)PyArray_DATA(*output3), N-1, J);
